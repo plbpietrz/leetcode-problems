bd13c333e2c81c1fc01a5e81a8604daa3d268414
Bartlomiej Pietrzyk <bartlomiej.pietrzyk@gmail.com>
2020-05-21 23:24:49 +0200

    Problem 3 solution 2

--- src/main/java/rhx/leetcode/Problem3LongestSubstring.java
+++ src/main/java/rhx/leetcode/Problem3LongestSubstring.java
@@ -1,30 +1,24 @@
 package rhx.leetcode;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.Arrays;
 
 import static java.lang.Math.max;
 
 public class Problem3LongestSubstring {
 
     public int lengthOfLongestSubstring(String s) {
-        int result = 0, seqStart = 0, seqEnd = 0;
-        char[] inputChars = s.toCharArray();
-        Set<Character> unique = new HashSet<>();
+        int result = 0;
+        int[] lastIndexOfChar = new int[256];    //lets hope input is only ascii
+        Arrays.fill(lastIndexOfChar, -1);
 
-        while (seqEnd < inputChars.length) {
-            if (!unique.add(inputChars[seqEnd])) {
-                result = max(result, seqEnd - seqStart);
-                while (inputChars[seqStart] != inputChars[seqEnd] && seqStart <= seqEnd) {
-                    unique.remove(inputChars[seqStart++]);
-                }
-                seqStart += 1;
-            }
-            seqEnd += 1;
+        int x = 0;
+        for (int i = 0; i < s.length(); ++i) {
+            x = max(x, lastIndexOfChar[s.charAt(i)] + 1);
+            result = max(result, i - x + 1);
+            lastIndexOfChar[s.charAt(i)] = i;
         }
-        
 
-        return max(result, seqEnd - seqStart);
+        return result;
     }
 
 }
