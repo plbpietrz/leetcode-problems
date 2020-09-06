7d7e9aadcaf0d68eae60389cab93aaea185ce3dc
Bartłomiej Pietrzyk <bartlomiej.pietrzyk@live.com>
2020-09-06 19:17:06 +0200

    Problem 835 - Image overlap

---
+++ src/test/java/rhx/leetcode/problems830to839/P835ImageOverlapTest.java
@@ -0,0 +1,46 @@
+package rhx.leetcode.problems830to839;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class P835ImageOverlapTest {
+
+    private P835ImageOverlap sut;
+
+    @BeforeEach
+    public void setup() {
+        sut = new P835ImageOverlap();
+    }
+
+    @Test
+    public void test_1() {
+        assertEquals(3, sut.largestOverlap(
+                new int[][] {
+                        {1, 1, 0},
+                        {0, 1, 0},
+                        {0, 1, 0}
+                },
+                new int[][] {
+                        {0, 0, 0},
+                        {0, 1, 1},
+                        {0, 0, 1}
+                }
+        ));
+    }
+
+    @Test
+    public void test_2() {
+        assertEquals(2, sut.largestOverlap(
+                new int[][] {
+                        {0, 1},
+                        {1, 1}
+                },
+                new int[][] {
+                        {1, 1},
+                        {1, 0}
+                }
+        ));
+    }
+}
