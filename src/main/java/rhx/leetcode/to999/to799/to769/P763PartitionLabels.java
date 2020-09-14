ef35a34275d73429ff144b2ed7f15d7b87a0a9de
CreateThreadEx <bartlomiej.pietrzyk@live.com>
2020-09-15 00:50:11 +0200

    Problem 763 - Partition lables

--- src/main/java/rhx/leetcode/to999/to799/to769/P763PartitionLabels.java
+++ src/main/java/rhx/leetcode/to999/to799/to769/P763PartitionLabels.java
@@ -1,12 +1,47 @@
 package rhx.leetcode.to999.to799.to769;
 
-import java.util.Collections;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 public class P763PartitionLabels {
 
     public List<Integer> partitionLabels(String S) {
-        return Collections.emptyList();
+        int[][] aux = new int[26][2];
+        for (int i = 0; i < aux.length; ++i) {
+            aux[i][0] = Integer.MAX_VALUE;
+        }
+        
+        char[] chars = S.toCharArray();
+        for (int i = 0; i < chars.length; ++i) {
+            int idx = chars[i] - 97;
+            if (aux[idx][0] == Integer.MAX_VALUE) {
+                aux[idx][0] = i;
+            }
+            aux[idx][1] = i;
+        }
+        
+        Arrays.sort(aux, Comparator.comparing(e -> e[0]));
+        
+        List<Integer> result = new ArrayList<>();
+        int min = 0, max = aux[0][1];
+        for (int i = 1; i < aux.length; ++i) {
+            int[] ch = aux[i];
+            
+            if (ch[0] == Integer.MAX_VALUE) {
+                break;
+            } else if (max < ch[0]) {
+                result.add(max - min + 1);
+                min = ch[0];
+                max = ch[1];
+            } else {
+                max = Math.max(max, ch[1]);
+            }
+        }
+        result.add(max - min + 1);
+        
+        return result;
     }
 
 }
--- src/test/java/rhx/leetcode/to999/to799/to769/P763PartitionLabelsTest.java
+++ src/test/java/rhx/leetcode/to999/to799/to769/P763PartitionLabelsTest.java
@@ -3,6 +3,7 @@ package rhx.leetcode.to999.to799.to769;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Collections;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -22,5 +23,23 @@ class P763PartitionLabelsTest {
                 List.of(9, 7, 8),
                 sut.partitionLabels("ababcbacadefegdehijhklij")
         );
+    }
+
+    @Test
+    void test_2() {
+        assertIterableEquals(
+                List.of(246, 254),
+                sut.partitionLabels("ntswuqqbidunnixxpoxxuuupotaatwdainsotwvpxpsdvdbwvbtdiptwtxnnbtqbdvnbowqitudutpsxsbbsvtipibqpvpnivottsxvoqqaqdxiviidivndvdtbvadnxboiqivpusuxaaqnqaobutdbpiosuitdnopoboivopaapadvqwwnnwvxndpxbapixaspwxxxvppoptqxitsvaaawxwaxtbxuixsoxoqdtopqqivaitnpvutzchkygjjgjkcfzjzrkmyerhgkglcyffezmehjcllmlrjghhfkfylkgyhyjfmljkzglkklykrjgrmzjyeyzrrkymccefggczrjflykclfhrjjckjlmglrmgfzlkkhffkjrkyfhegyykrzgjzcgjhkzzmzyejycfrkkekmhzjgggrmchkeclljlyhjkchmhjlehhejjyccyegzrcrerfzczfelzrlfylzleefgefgmzzlggmejjjygehmrczmkrc")
+        );
+    }
+
+    @Test
+    void test_3() {
+        assertIterableEquals(List.of(1, 1, 1), sut.partitionLabels("abc"));
+    }
+
+    @Test
+    void test_4() {
+        assertIterableEquals(List.of(1), sut.partitionLabels(""));
     }
 }
